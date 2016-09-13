ab4555d1cd20bf32285af3c6c50c923bd1c81ca3
Yunhao Ji <yunhao.ji@mail.utoronto.ca>
2016-09-12 20:43:14 -0400

    adding first three files

--- warmup/fact.c
+++ warmup/fact.c
@@ -1,8 +1,41 @@
 #include "common.h"
+#include <stdio.h>
+#include <unistd.h>
+int fact(int base);
+int isFloat(float num);
 
 int
-main()
+main(int argc, char *argv[])
 {
-	TBD();
+	//TBD();
+        if (argc == 2) {
+            float num = atof(argv[1]);
+            if (num <= 0 || isFloat(num) == 1) {
+                printf("Huh?\n");
+                return 0;
+            } else if (num > 12) {
+                printf("Overflow\n");
+                return 0;
+            }
+            int res = fact(num);
+            printf("%d", res);
+            printf("\n");
+        } else {
+            printf("Huh?\n");
+        }
+
 	return 0;
+}
+
+int fact(int base) {
+    if (base == 1) return base;
+    else {
+        return base*fact(base-1);
+    }
+}
+
+int isFloat(float num) {
+    int temp = num;
+    if ( (num-temp) != 0 ) return 1;
+    else return 0;
 }
--- warmup/words.c
+++ warmup/words.c
@@ -1,8 +1,16 @@
 #include "common.h"
+#include <stdio.h>
 
 int
-main()
+main(int argc, char *argv[])
 {
-	TBD();
+	//TBD();
+	if (argc > 1) {
+	    int i;
+	    for (i=1;i<argc;i++) {
+	        printf(argv[i]);
+	        printf("\n");
+	    }
+	}
 	return 0;
 }
